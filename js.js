9551e555d5e88db5d4db0d8311ac85d2b52fa5b5
monkey <iamsamspike@gmail.com>
2019-07-11 15:09:18 +1000

    comment

--- js.js
+++ js.js
@@ -8,10 +8,17 @@ var square7 = document.querySelector(".square7")
 var square8 = document.querySelector(".square8")
 var square9 = document.querySelector(".square9")
 var board = document.querySelector(".board")
-var boarddivs = document.querySelectorAll(".board div")
-var sqaures = document.querySelectorAll('div')
+var boarddivs = document.querySelectorAll(".board, div")
+var squares = document.querySelectorAll('main div')
+var randomSquare = document.querySelector('.square' + coinLocation)
 
+var coinLocation = Math.floor((Math.random() * 8) + 0)
+
+
+var previousSquareClicked = ''
 var playerTurn = true
+var i = 'x'
+
 var boardArray = ['','','',
                 '','','',
                 '','',''] 
@@ -23,9 +30,17 @@ var XorO = function () {
         i = 'o'
     }
 }
+var generateCoin = function() { 
+    var coinChance = Math.floor((Math.random() * 3) + 1)
+ if (coinChance === 3) {
+    var coinLocation = Math.floor((Math.random() * 8) + 0)
+    
+    }
+}
 
 var handleClick = function(event) {
     XorO()
+    clickOccupied()
     boardArray[Number(event.target.dataset.cell)] = i
     var solutions = [
         [i,i,i, // solution 1
@@ -53,19 +68,328 @@ var handleClick = function(event) {
         'I','I',i,
         'I','I',i]
     ]
-
-    if (event.target.textContent === '') {
+    if (event.target.classList.contains('chessTake')){
+        event.target.textContent = i
+        previousSquareClicked.textContent = ''
+        boardArray[Number(previousSquareClicked.dataset.cell)] = ''
         event.target.textContent = i
         solutions.forEach(compare)
         playerTurn = !playerTurn
+        square1.classList.remove('chessMove', 'chessTake');
+        square2.classList.remove('chessMove', 'chessTake');
+        square3.classList.remove('chessMove', 'chessTake');
+        square4.classList.remove('chessMove', 'chessTake');
+        square5.classList.remove('chessMove', 'chessTake');
+        square6.classList.remove('chessMove', 'chessTake');
+        square7.classList.remove('chessMove', 'chessTake');
+        square8.classList.remove('chessMove', 'chessTake');
+        square9.classList.remove('chessMove', 'chessTake');
+    }
+    if (event.target.textContent === '') {
+        if (event.target.classList.contains('chessMove')){
+            previousSquareClicked.textContent = ''
+            boardArray[Number(previousSquareClicked.dataset.cell)] = ''
+        }
+        event.target.textContent = i
+        solutions.forEach(compare)
+        playerTurn = !playerTurn
+        square1.classList.remove('chessMove', 'chessTake');
+        square2.classList.remove('chessMove', 'chessTake');
+        square3.classList.remove('chessMove', 'chessTake');
+        square4.classList.remove('chessMove', 'chessTake');
+        square5.classList.remove('chessMove', 'chessTake');
+        square6.classList.remove('chessMove', 'chessTake');
+        square7.classList.remove('chessMove', 'chessTake');
+        square8.classList.remove('chessMove', 'chessTake');
+        square9.classList.remove('chessMove', 'chessTake');
+    } else {
+        previousSquareClicked = event.target
     }
 }
 
-// var baordArray = 
-// ['o','o','x',
-// 'x','x',' ',
-// ' ',' ','o']
+var clickOccupied = function() {
+    // x on square1
+    if (event.target === square1 && square1.textContent === 'x' && i === 'x' && square2.textContent === "") {
+        square2.classList.toggle('chessMove');
+    }
+    if (event.target === square1 && square1.textContent === 'x' && i === 'x' && square4.textContent === "") {
+        square4.classList.toggle('chessMove');
+    }
+    // x on square1 takes
+    if (event.target === square1 && square1.textContent === 'x' && i === 'x' && square5.textContent === 'o') {
+        square5.classList.toggle('chessTake');
+    }
+    // x on square2
+    if (event.target === square2 && square2.textContent === 'x' && i === 'x' && square1.textContent === "") {
+        square1.classList.toggle('chessMove');
+    }
+    if (event.target === square2 && square2.textContent === 'x' && i === 'x' && square3.textContent === "") {
+        square3.classList.toggle('chessMove');
+    }
+    if (event.target === square2 && square2.textContent === 'x' && i === 'x' && square5.textContent === "") {
+        square5.classList.toggle('chessMove');
+    }
+    // x on square2 takes
+    if (event.target === square2 && square2.textContent === 'x' && i === 'x' && square4.textContent === 'o') {
+        square4.classList.toggle('chessTake');
+    }
+    if (event.target === square2 && square2.textContent === 'x' && i === 'x' && square6.textContent === 'o') {
+        square6.classList.toggle('chessTake');
+    }
+    // x on square3
+    if (event.target === square3 && square3.textContent === 'x' && i === 'x' && square2.textContent === "") {
+        square2.classList.toggle('chessMove');
+    }
+    if (event.target === square3 && square3.textContent === 'x' && i === 'x' && square6.textContent === "") {
+        square6.classList.toggle('chessMove');
+    }
+    // x on square3 takes
+    if (event.target === square3 && square3.textContent === 'x' && i === 'x' && square5.textContent === 'o') {
+        square5.classList.toggle('chessTake');
+    }
+    // x on square4
+    if (event.target === square4 && square4.textContent === 'x' && i === 'x' && square5.textContent === "") {
+        square5.classList.toggle('chessMove');
+    }
+    if (event.target === square4 && square4.textContent === 'x' && i === 'x' && square1.textContent === "") {
+        square1.classList.toggle('chessMove');
+    }
+    if (event.target === square4 && square4.textContent === 'x' && i === 'x' && square7.textContent === "") {
+        square7.classList.toggle('chessMove');
+    }
+    // x on square4 takes
+    if (event.target === square4 && square4.textContent === 'x' && i === 'x' && square2.textContent === 'o') {
+        square2.classList.toggle('chessTake');
+    }
+    if (event.target === square4 && square4.textContent === 'x' && i === 'x' && square8.textContent === 'o') {
+        square8.classList.toggle('chessTake');
+    }
+    // x on square5
+    if (event.target === square5 && square5.textContent === 'x' && i === 'x' && square2.textContent === "") {
+        square2.classList.toggle('chessMove');
+    }
+    if (event.target === square5 && square5.textContent === 'x' && i === 'x' && square4.textContent === "") {
+        square4.classList.toggle('chessMove');
+    }
+    if (event.target === square5 && square5.textContent === 'x' && i === 'x' && square6.textContent === "") {
+        square6.classList.toggle('chessMove');
+    }
+    if (event.target === square5 && square5.textContent === 'x' && i === 'x' && square8.textContent === "") {
+        square8.classList.toggle('chessMove');
+    }
+    // x on square5 takes
+    if (event.target === square5 && square5.textContent === 'x' && i === 'x' && square1.textContent === 'o') {
+        square1.classList.toggle('chessTake');
+    }
+    if (event.target === square5 && square5.textContent === 'x' && i === 'x' && square3.textContent === 'o') {
+        square3.classList.toggle('chessTake');
+    }
+    if (event.target === square5 && square5.textContent === 'x' && i === 'x' && square7.textContent === 'o') {
+        square7.classList.toggle('chessTake');
+    }
+    if (event.target === square5 && square5.textContent === 'x' && i === 'x' && square9.textContent === 'o') {
+        square9.classList.toggle('chessTake');
+    }
+    // x on square6
+    if (event.target === square6 && square6.textContent === 'x' && i === 'x' && square5.textContent === "") {
+        square5.classList.toggle('chessMove');
+    }
+    if (event.target === square6 && square6.textContent === 'x' && i === 'x' && square3.textContent === "") {
+        square3.classList.toggle('chessMove');
+    }
+    if (event.target === square6 && square6.textContent === 'x' && i === 'x' && square9.textContent === "") {
+        square9.classList.toggle('chessMove');
+    }
+    // x on square5 take6
+    if (event.target === square6 && square6.textContent === 'x' && i === 'x' && square2.textContent === 'o') {
+        square2.classList.toggle('chessTake');
+    }
+    if (event.target === square6 && square6.textContent === 'x' && i === 'x' && square8.textContent === 'o') {
+        square8.classList.toggle('chessTake');
+    }
+    // x on sqaure7
+    if (event.target === square7 && square7.textContent === 'x' && i === 'x' && square4.textContent === "") {
+        square4.classList.toggle('chessMove');
+    }
+    if (event.target === square7 && square7.textContent === 'x' && i === 'x' && square8.textContent === "") {
+        square8.classList.toggle('chessMove');
+    }
+    // x on square7 takes
+    if (event.target === square7 && square7.textContent === 'x' && i === 'x' && square5.textContent === 'o') {
+        square5.classList.toggle('chessTake');
+    }
+    // x on square8
+    if (event.target === square8 && square8.textContent === 'x' && i === 'x' && square5.textContent === "") {
+        square5.classList.toggle('chessMove');
+    }
+    if (event.target === square8 && square8.textContent === 'x' && i === 'x' && square7.textContent === "") {
+        square7.classList.toggle('chessMove');
+    }
+    if (event.target === square8 && square8.textContent === 'x' && i === 'x' && square9.textContent === "") {
+        square9.classList.toggle('chessMove');
+    }
+    // x on square8 takes
+    if (event.target === square8 && square8.textContent === 'x' && i === 'x' && square4.textContent === 'o') {
+        square4.classList.toggle('chessTake');
+    }
+    if (event.target === square8 && square8.textContent === 'x' && i === 'x' && square6.textContent === 'o') {
+        square6.classList.toggle('chessTake');
+    }
+    // x on square9
+    if (event.target === square9 && square9.textContent === 'x' && i === 'x' && square6.textContent === "") {
+        square6.classList.toggle('chessMove');
+    }
+    if (event.target === square9 && square9.textContent === 'x' && i === 'x' && square8.textContent === "") {
+        square8.classList.toggle('chessMove');
+    }
+    // x on square9 takes
+    if (event.target === square9 && square9.textContent === 'x' && i === 'x' && square5.textContent === 'o') {
+        square5.classList.toggle('chessTake');
+    }
+    // o on square1
+    if (event.target === square1 && square1.textContent === 'o' && i === 'o' && square2.textContent === "") {
+        square2.classList.toggle('chessMove');
+    }
+    if (event.target === square1 && square1.textContent === 'o' && i === 'o' && square4.textContent === "") {
+        square4.classList.toggle('chessMove');
+    }
+    // o on square1 takes
+    if (event.target === square1 && square1.textContent === 'o' && i === 'o' && square5.textContent === 'x') {
+        square5.classList.toggle('chessTake');
+    }
+    // o on square2
+    if (event.target === square2 && square2.textContent === 'o' && i === 'o' && square1.textContent === "") {
+        square1.classList.toggle('chessMove');
+    }
+    if (event.target === square2 && square2.textContent === 'o' && i === 'o' && square3.textContent === "") {
+        square3.classList.toggle('chessMove');
+    }
+    if (event.target === square2 && square2.textContent === 'o' && i === 'o' && square5.textContent === "") {
+        square5.classList.toggle('chessMove');
+    }
+    // o on square2 takes
+    if (event.target === square2 && square2.textContent === 'o' && i === 'o' && square4.textContent === 'x') {
+        square4.classList.toggle('chessTake');
+    }
+    if (event.target === square2 && square2.textContent === 'o' && i === 'o' && square6.textContent === 'x') {
+        square6.classList.toggle('chessTake');
+    }
+    // o on square3
+    if (event.target === square3 && square3.textContent === 'o' && i === 'o' && square2.textContent === "") {
+        square2.classList.toggle('chessMove');
+    }
+    if (event.target === square3 && square3.textContent === 'o' && i === 'o' && square6.textContent === "") {
+        square6.classList.toggle('chessMove');
+    }
+    // o on square3 takes
+    if (event.target === square3 && square3.textContent === 'o' && i === 'o' && square5.textContent === 'x') {
+        square5.classList.toggle('chessTake');
+    }
+    // o on square4
+    if (event.target === square4 && square4.textContent === 'o' && i === 'o' && square5.textContent === "") {
+        square5.classList.toggle('chessMove');
+    }
+    if (event.target === square4 && square4.textContent === 'o' && i === 'o' && square1.textContent === "") {
+        square1.classList.toggle('chessMove');
+    }
+    if (event.target === square4 && square4.textContent === 'o' && i === 'o' && square7.textContent === "") {
+        square7.classList.toggle('chessMove');
+    }
+    // o on square4 takes
+    if (event.target === square4 && square4.textContent === 'o' && i === 'o' && square2.textContent === 'x') {
+        square2.classList.toggle('chessTake');
+    }
+    if (event.target === square4 && square4.textContent === 'o' && i === 'o' && square8.textContent === 'x') {
+        square8.classList.toggle('chessTake');
+    }
+    // o on square5
+    if (event.target === square5 && square5.textContent === 'o' && i === 'o' && square2.textContent === "") {
+        square2.classList.toggle('chessMove');
+    }
+    if (event.target === square5 && square5.textContent === 'o' && i === 'o' && square4.textContent === "") {
+        square4.classList.toggle('chessMove');
+    }
+    if (event.target === square5 && square5.textContent === 'o' && i === 'o' && square6.textContent === "") {
+        square6.classList.toggle('chessMove');
+    }
+    if (event.target === square5 && square5.textContent === 'o' && i === 'o' && square8.textContent === "") {
+        square8.classList.toggle('chessMove');
+    }
+    // x on square5 takes
+    if (event.target === square5 && square5.textContent === 'o' && i === 'o' && square1.textContent === 'x') {
+        square1.classList.toggle('chessTake');
+    }
+    if (event.target === square5 && square5.textContent === 'o' && i === 'o' && square3.textContent === 'x') {
+        square3.classList.toggle('chessTake');
+    }
+    if (event.target === square5 && square5.textContent === 'o' && i === 'o' && square7.textContent === 'x') {
+        square7.classList.toggle('chessTake');
+    }
+    if (event.target === square5 && square5.textContent === 'o' && i === 'o' && square9.textContent === 'x') {
+        square9.classList.toggle('chessTake');
+    }
+    // o on square6
+    if (event.target === square6 && square6.textContent === 'o' && i === 'o' && square5.textContent === "") {
+        square5.classList.toggle('chessMove');
+    }
+    if (event.target === square6 && square6.textContent === 'o' && i === 'o' && square3.textContent === "") {
+        square3.classList.toggle('chessMove');
+    }
+    if (event.target === square6 && square6.textContent === 'o' && i === 'o' && square9.textContent === "") {
+        square9.classList.toggle('chessMove');
+    }
+    // x on square6 takes
+    if (event.target === square6 && square6.textContent === 'o' && i === 'o' && square2.textContent === 'x') {
+        square2.classList.toggle('chessTake');
+    }
+    if (event.target === square6 && square6.textContent === 'o' && i === 'o' && square8.textContent === 'x') {
+        square8.classList.toggle('chessTake');
+    }
+    // o on sqaure7
+    if (event.target === square7 && square7.textContent === 'o' && i === 'o' && square4.textContent === "") {
+        square4.classList.toggle('chessMove');
+    }
+    if (event.target === square7 && square7.textContent === 'o' && i === 'o' && square8.textContent === "") {
+        square8.classList.toggle('chessMove');
+    }
+    // x on square7 takes
+    if (event.target === square7 && square7.textContent === 'o' && i === 'o' && square5.textContent === 'x') {
+        square5.classList.toggle('chessTake');
+    }
+    // o on square8
+    if (event.target === square8 && square8.textContent === 'o' && i === 'o' && square5.textContent === "") {
+        square5.classList.toggle('chessMove');
+    }
+    if (event.target === square8 && square8.textContent === 'o' && i === 'o' && square7.textContent === "") {
+        square7.classList.toggle('chessMove');
+    }
+    if (event.target === square8 && square8.textContent === 'o' && i === 'o' && square9.textContent === "") {
+        square9.classList.toggle('chessMove');
+    }
+    // x on square8 takes
+    if (event.target === square8 && square8.textContent === 'o' && i === 'o' && square4.textContent === 'x') {
+        square4.classList.toggle('chessTake');
+    }
+    if (event.target === square8 && square8.textContent === 'o' && i === 'o' && square6.textContent === 'x') {
+        square6.classList.toggle('chessTake');
+    }
+    // o on square9
+    if (event.target === square9 && square9.textContent === 'o' && i === 'o' && square6.textContent === "") {
+        square6.classList.toggle('chessMove');
+    }
+    if (event.target === square9 && square9.textContent === 'o' && i === 'o' && square8.textContent === "") {
+        square8.classList.toggle('chessMove');
+    }
+    // o on square9 takes
+    if (event.target === square9 && square9.textContent === 'o' && i === 'o' && square5.textContent === 'x') {
+        square5.classList.toggle('chessTake');
+    }
+} 
 
+
+
+
+// check winner
 var compare = function(arr){
     var counter = 0
     arr.forEach(function(cell, index) {
@@ -80,22 +404,6 @@ var compare = function(arr){
 
 
 
-// solution1.forEach(function(cell) {
-//     if (cell !== 'I') { 
-//     var solstr = solution1.join()
-//     var baostr = baordArray.join()
-//         if (solstr === baostr) {
-//             console.log('test')
-//         }
-//     }
-// })
 
-// solution 2
-// ['ingor','ignor','ingor',
-// 'i','i','i',
-// 'ignor', 'ignor','ingor']
-
-
-
-
+// board.addEventListener('click', clickOccupied)
 board.addEventListener('click', handleClick)
